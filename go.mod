ae5f49963a8bf3a56d9e993a4fb90409898dc3e3
twalker <tim.twalker@gmail.com>
2023-07-23 18:57:14 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/twalker/toolkit
+
+go 1.20
---
+++ tools.go
@@ -0,0 +1,22 @@
+package toolkit
+
+import "crypto/rand"
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
+// Tools is the type to instantiate this module. Ay variablee of this type will have access
+// to all the functions of this module.
+type Tools struct {
+}
+
+// RandomString generates a random string of length
+func (t *Tools) RandomString(length int) string {
+	s, r := make([]rune, length), []rune(randomStringSource)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
